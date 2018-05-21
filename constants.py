10ac664841c1aa2e3a443502de2d35884d96038a
Lars Bahner <lars.bahner@gmail.com>
2018-05-21 16:45:41 +0200

    Have a more easily read and edited file for contstants and maps

---
+++ constants.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2.7
+attribute_map = {
+    'address1': 'Adresse',
+    'email': 'E-post',
+    'firstname': 'Fornavn',
+    'lastname': 'Etternavn',
+    'phonemobile': 'Tlf. mobil',
+    'postaladdress': 'Postadresse',
+    'postalcode': 'Postnr',
+    'userid': 'Tlf. mobil',
+}
+
+default_values = {
+    'E-post': 'mangler.epost@opk.no',
+    'Adresse': 'Konglebakken 1'
+    'Postnr': '1111'
+    'Postadresse': 'BORTENFOR SOL OG MÅNE'
+}
